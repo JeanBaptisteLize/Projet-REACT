de236258455ddc4f00eee96d124126e2d62a075c
Jean-Baptiste Lizé <jean-baptiste.lize@isen.yncrea.fr>
2026-01-16 10:08:13 +0100

    projet react asynchrone fonctionnel

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,19 @@ export default function App() {
       setError(null);
 
       try {
-        const data = await fetchAllCountries();
+        const { response, data } = await fetchAllCountries();  // on recup response et data du fichier restCountries.js
 
         // Vous pouvez limiter à 20 pour un premier rendu (plus fluide)
         setCountries(data.slice(0, 20));
+
+        // 5) données visibles dans la console (console navigateur)
+        console.log("Données REST Countries:", data);
+
+        // Bonus: afficher juste 3 noms pour vérifier rapidement
+        console.log(
+          "Exemples (3 pays):",
+          data.slice(0, 3).map((c) => c?.name?.common)
+        );
       } catch (err) {
         setError(err.message || "Erreur inconnue");
         setCountries([]);     // important : garantit le cas "aucune donnée"
--- src/api/restCountries.js
+++ src/api/restCountries.js
@@ -14,7 +14,10 @@ Récupère tous les pays (avec champs limités).
 export async function fetchAllCountries() {
   const url = `${BASE_URL}/all?fields=${FIELDS}`;
 
-  const response = await fetch(url);
+  console.log("avant fetch");
+  const response = await fetch(url);  // fetch : Fonction JavaScript native qui envoie une requête HTTP à l'URL donnée
+                                      // Attrubue à response la confirmation de la promesse d'avoir reçu une réponse pour cette requête.
+  console.log("après fetch");
 
   // fetch() ne lance pas d'erreur automatiquement sur 404/500,
   // donc on teste response.ok. :contentReference[oaicite:1]{index=1}
@@ -22,6 +25,13 @@ export async function fetchAllCountries() {
     throw new Error(`REST Countries: HTTP ${response.status} ${response.statusText}`);
   }
 
-  // Parsing JSON (asynchrone). :contentReference[oaicite:2]{index=2}
-  return response.json();
+  //  Analyse la réponse du body as JSON (asynchrone). convertit le text brut en objet JS.
+  // comme c'est une promesse(asynchrone), il attend que le JSON soit prêt.
+
+  const data = await response.json();
+
+
+  // Retourne response et data pour avoir accès aux deux
+  return { response, data };
+
 }
